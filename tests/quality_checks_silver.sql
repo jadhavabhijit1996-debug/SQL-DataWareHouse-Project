aceef36d3c3c9b91962c4b27b93a184c2756325d
jadhavabhijit1996-debug <jadhavabhijit1996@gmail.com>
2026-09-25 20:48:54 +0530

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,3 +1,21 @@
+/*
+==================================================================================================
+Quality Checks
+==================================================================================================
+Script Purpose:
+	This script performs various quality checks for data consistency, accuracy and standardization
+	across the 'silver' schema. It includes checks for:
+	-Null or duplicate primary keys.
+	-Unwanted spaces in string fields.
+	-Data standardization and consistency.
+	-Invalid date ranges and orders.
+	-Data consistency between related fields.
+
+Usage Notes:
+	-Run these checks after data loading Silver layer.
+	-Investigate and resolve any discrepancies found during the checks.
+==================================================================================================
+*/
 
 SELECT
 prd_id,
